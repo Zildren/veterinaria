2ed62bec7d0ea9c27647586d46ff612f1a4aa648
Zildren <richy.pined4@gmail.com>
2025-05-17 22:20:04 -0600

    primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+  ],
+})
+export class HomePage {
+  constructor() {}
+}
